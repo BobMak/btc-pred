8571808aaba8483d7e6c74de0f238b8610bdb6ad
Pratik korat <pratikkorat1@gmail.com>
2024-05-18 13:00:09 -0700

    UI changes

--- deployment/deploy.py
+++ deployment/deploy.py
@@ -1,58 +1,68 @@
+
+import pandas as pd
 import streamlit as st
-from datetime import datetime
+from datetime import datetime, timedelta
+from load_data import MODEL_PATH, FILE_PATH
 from load_data import load_btc_data, read_preprocessor, create_model
 from load_data import predict_future_values
-from load_data import MODEL_PATH, FILE_PATH
 
 model = create_model(MODEL_PATH)
 processor = read_preprocessor(FILE_PATH)
 
 
-def create_app():
+def create_app(num_predictions=7):
     st.title("Bitcoin Price Prediction")
 
     ## To ensure tha date other than current date is not selected
     today = datetime.today().date()
     date = st.date_input("Select the date", max_value=today)
+    last_predicted_date = today + timedelta(days=num_predictions)
     sel_date = date.strftime('%Y-%m-%d')
+
 
     st.write("You selected date: ", sel_date)
 
     data = load_btc_data(sel_date)
 
+    # to ensure we'll load the bitcoin using that
+    openprices = data["Open"]
+    data = data.drop(columns=["Open"])
+
     if st.button("Predict"):
         predictions = predict_future_values(model, processor, data, data.index.tolist(), num_predictions=7)
 
-        st.write("Predicted price(USD) for BitCoin for the next 7 days:")
+        st.subheader("Predicted price(USD) for BitCoin for the next 7 days:")
 
         max_high = predictions['High'].max()
         min_low = predictions['Low'].min()
         avg_close = predictions['Close'].mean()
 
-        st.write("Predicted High: ", max_high)
-        st.write("Predicted Low: ", min_low)
-        st.write("Predicted Average Close: ", avg_close)
+        predicted_data = pd.DataFrame(data={"predictions": [max_high, min_low, avg_close]},
+                                      index=["Max High", "Min Low", "Avg Close"])
 
-        max_high_index = predictions['High'].idxmax().strftime('%Y-%m-%d')
-        min_low_index = predictions['Low'].idxmin().strftime('%Y-%m-%d')
+        st.dataframe(predicted_data)
+
+        max_high_index = predictions['High'].idxmax()
+        min_low_index = predictions['Low'].idxmin()
 
         st.subheader("Recommended Swing Strategy:")
+
         sell_date = None
         load_date = None
+
         if max_high_index < min_low_index:
             sell_date = max_high_index
-            load_date = min_low_index
-        elif max_high_index == today:
+            load_date = min_low_index if min_low_index != last_predicted_date else None
+            # He bought? Dump it
+        elif max_high_index == today and min_low_index != today:
             sell_date = today
-        elif min_low_index < max_high_index and (max_high_index != (today + datetime.diff)):
-            sell_date = min_low_index
-            load_date = max_high_index#
-            pass
+            # bitcoin will peak later this week
+        elif min_low_index < max_high_index:
+            # hold if the price will be going up
+            sell_date = max_high_index if max_high_index != last_predicted_date else None
 
-        st.write("Predicted Highest High: ", max_high)
-        st.write("Predicted Lowest Low: ", min_low)
-        st.write("Predicted Highest High day: ", max_high_index)
-        st.write("Predicted Lowest Low day: ", min_low_index)
+        sell_data = pd.DataFrame(data={"Strategy": [sell_date, load_date]}, index=["Sell Date", "Load Date"])
+        st.dataframe(sell_data)
 
 
 if __name__ == '__main__':
